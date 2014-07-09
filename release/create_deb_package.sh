8518b2779a3b2129467e5b2be193320893061139
ruben <ruben.git@r.z11.de>
2014-07-09 11:48:17 +0200

    debian packaging installation script added

---
+++ release/create_deb_package.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#A debian package can be created calling this script inside the package
+PACKAGE=mouse-speed
+TMPFOLDER=/tmp/dpkg-packaging
+
+echo
+echo '#'cleaning up $TMPFOLDER and recreate it with the needed files
+
+( set -x
+	rm -Rf $TMPFOLDER/$PACKAGE
+	#create build dir
+	mkdir -p $TMPFOLDER/$PACKAGE/
+	#copy files
+	cp -r DEBIAN $TMPFOLDER/$PACKAGE/
+	cp -r usr $TMPFOLDER/$PACKAGE/
+	#
+	cd $TMPFOLDER
+)
+
+echo
+echo building package ...
+
+( set -x
+dpkg-deb -z8 -Zgzip --build $TMPFOLDER/$PACKAGE
+ls -lah $TMPFOLDER
+)
+echo install with:
+echo sudo dpkg -i /tmp/dpkg-packaging/mouse-speed.deb
